1a3aed9b60493ca81c008e1a0312d24e2e94cbca
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-10-27 14:59:19 -0400

    navbar-added

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar__title">Red Application</div>
+            <div className="navbar__menu">
+                <a href="#" className="navbar__link">Home</a>
+                <a href="#" className="navbar__link">Communities</a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
