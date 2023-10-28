7e8daa446c53d8451087ed2c44e3c1f5a5686112
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-10-28 16:04:00 -0400

    app will fetch random url provided in app.js from 5 subreddits and display posts randomy on every page refresh. Trying to find way to display jpg from github api.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,28 @@ export default function App() {
     const [redditData, setRedditData] = useState({});
     const [error, setError] =useState(false)
 
+    function randomFetchUrl() {
+        const randomNum = Math.floor(Math.random() *5)
+
+        if (randomNum === 0) {
+            return 'https://www.reddit.com/r/GameStop/top.json?limit=10'
+        } else if (randomNum === 1) {
+            return 'https://www.reddit.com/r/apple/top.json?limit=10'
+        } else if (randomNum === 2) {
+            return 'https://www.reddit.com/r/apple/top.json?limit=10'
+        } else if (randomNum === 3) {
+            return 'https://www.reddit.com/r/cars/top.json?limit=10'
+        } else if (randomNum === 4) {
+            return 'https://www.reddit.com/r/Minecraft/top.json?limit=10'
+        } else if (randomNum === 5) {
+            return 'https://www.reddit.com/r/Battlefield/top.json?limit=10'
+        } else {
+            return false
+        }
+    }
+
     useEffect(() => {
-        fetch('https://www.reddit.com/r/GameStop/top.json?limit=10')
+        fetch(randomFetchUrl())
         .then((response) => response.json())
         .then((jsonData) => setRedditData(jsonData))
         .catch((error) => setError(true))
--- src/components/Main.js
+++ src/components/Main.js
@@ -11,15 +11,16 @@ function Main(props) {
                 <p>Subreddit: {post.data.subreddit_name_prefixed}</p>
                 <h4>Title:<br/> {post.data.title}</h4>
                 <p>{post.data.selftext}</p>
+                {post.data.secure_media ? (<video src={post.data.secure_media.reddit_video.scrubber_media_url} controls width="320" height="240" autoplay/>) : "" }
             </div>
         );
     };
 
-    // console.log(renderPost())
     return (
         <div className="main-posts">
-            {props.error && <p>Error</p>}
-            {!props.error && data.length === 0 && <p>Fetching... Please wait</p>}
+            {props.error && <p>Error on fetch</p>}
+            {props.redditData.error ? <p>Too many requests: Error 429</p> : ""}
+            {!props.error && data.length === 0 && !props.redditData.error && <p>Fetching... Please wait</p>}
             {data.map((post) => renderPost(post))}
         </div>
     )
